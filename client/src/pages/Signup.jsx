02cc2a8118e407d1a9f9c535cb21752155be676a
AkithJayalath <akithjayalath@gmail.com>
2024-05-06 10:22:26 +0530

    create signup page ui

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,69 @@
 //import React from 'react'
 
+import { Button, Label, TextInput } from "flowbite-react";
+import { Link } from "react-router-dom";
+
 export default function Signup() {
   return (
-    <div>Signup</div>
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left */}
+        <div className="flex-1">
+        <Link
+            to='/' 
+            className='font-bold dark:text-white text-4xl'
+        >
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500
+            via-purple-500 to-pink-500 rounded-lg text-white'>
+             Sahand's
+             </span>
+            Blog
+        </Link>
+        <p className="text-sm mt-5">
+          This is a demo project. You can sign up with tour email and password
+          or with google
+        </p>
+        </div>
+        {/* right */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value="Your username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username" 
+                style={{width:'398px'}} />
+            </div>
+            <div>
+              <Label value="Your email" />
+              <TextInput
+                type="text"
+                placeholder="Email"
+                id="email" 
+                style={{width:'398px'}} />
+            </div>
+            <div>
+              <Label value="Your password" />
+              <TextInput
+                type="text"
+                placeholder="Password"
+                id="password" 
+                style={{width:'398px'}} />
+            </div>
+            <Button type="submit" className="px-2 py-1 bg-gradient-to-r from-indigo-500
+            via-purple-500 to-pink-500 rounded-lg text-white">
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to='/sign-in' className="text-blue-500 ">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+      </div>
   )
 }
