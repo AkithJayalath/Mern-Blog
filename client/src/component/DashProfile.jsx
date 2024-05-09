1ef9c1bc03709a57d8beffadd8ea3e20a09aabe6
AkithJayalath <akithjayalath@gmail.com>
2024-05-09 21:50:48 +0530

    changeed rules in firebase to correct error

--- client/src/component/DashProfile.jsx
+++ client/src/component/DashProfile.jsx
@@ -33,7 +33,8 @@ export default function DashProfile() {
     service firebase.storage {
   match /b/{bucket}/o {
     match /{allPaths=**} {
-      allow read, write: if
+      allow read;
+      allow write: if
       request.resource.size < 2*1024*1024 &&
       request.resource.contentType.matches('image/.*')
     }
